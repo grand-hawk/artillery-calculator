d946cb00d5bddd34de377596488e3f9f1ebb998a
Astrid <git@astrid.email>
2024-06-01 01:13:09 +0200

    add header to cache full size images for an hour

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  async headers() {
+    return [
+      {
+        source: '/images/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=3600, must-revalidate',
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default config;
