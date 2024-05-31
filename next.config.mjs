86e92842516bc2a9d91a2666c732c759f304d227
Astrid <git@astrid.email>
2024-05-31 22:46:58 +0200

    remove bundle analyzer

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
-import bundleAnalyzer from '@next/bundle-analyzer';
-
 /** @type {import("next").NextConfig} */
 const config = {};
 
-export default bundleAnalyzer({
-  enabled: process.env.NODE_ENV !== 'development',
-  openAnalyzer: false,
-})(config);
+export default config;
