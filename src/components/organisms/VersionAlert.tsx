3c93dac178abe64284c5490a1ac67b11c68534e6
Astrid <git@astrid.email>
2024-06-03 00:42:11 +0200

    fix version alert

--- src/components/organisms/VersionAlert.tsx
+++ src/components/organisms/VersionAlert.tsx
@@ -16,16 +16,12 @@ export default function VersionAlert({
     // with that in mind, we fetch the /api/version endpoint which always returns
     // the latest version production is on
 
-    const interval = setInterval(
-      async () => {
-        const remoteVersion = await getRemoteVersion();
+    const interval = setInterval(async () => {
+      const remoteVersion = await getRemoteVersion();
 
-        if (remoteVersion && remoteVersion !== currentVersion)
-          setUpdateAvailable(true);
-      },
-      // 2.5 minutes
-      2.5 * 60 * 1_000,
-    );
+      if (remoteVersion && remoteVersion !== currentVersion)
+        setUpdateAvailable(true);
+    }, 60 * 1_000);
 
     return () => clearInterval(interval);
   });
--- src/lib/client/getRemoteVersion.ts
+++ src/lib/client/getRemoteVersion.ts
@@ -3,7 +3,7 @@ import ky from 'ky';
 export default async function getRemoteVersion(): Promise<string | null> {
   const response = await ky.get('/api/version');
 
-  if (response.ok) return null;
+  if (!response.ok) return null;
 
   const data = (await response.json()) as { version: string };
 
