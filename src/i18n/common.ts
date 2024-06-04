359d159a8a5176c154636d733ff66474819a9767
Astrid <git@astrid.email>
2024-06-04 18:44:32 +0200

    i18n stuff and bundle analyzer

--- src/i18n/common.ts
+++ src/i18n/common.ts
@@ -1,10 +1,9 @@
 /* eslint sort-keys: "error" */
-
 const common = {
   languages: {
     'de-DE': 'Deutsch',
     'en-US': 'English',
-    'es-ES': 'Polski',
+    'es-ES': 'Español',
     'pl-PL': 'Polski',
     'ru-RU': 'Русский',
     'uk-UA': 'українська',
