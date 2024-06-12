60a1b23aee5f6382f15060be0166a10450e2125f
Astrid <git@astrid.email>
2024-06-13 01:15:57 +0200

    remove debug log statement

--- src/hooks/useAdBlockTrack.ts
+++ src/hooks/useAdBlockTrack.ts
@@ -55,7 +55,5 @@ export default function useAdBlockTrack(): boolean {
       clientHasAdBlock();
   }, [localStorage, setLocalStorage]);
 
-  console.log(localStorage);
-
   return localStorage.value;
 }
