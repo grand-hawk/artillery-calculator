0546651b66251cd4b963276b4f59e7ce88d3b945
Astrid <git@astrid.email>
2024-06-20 20:35:25 +0200

    overlay lol

---
+++ packages/mtc-artillery-overlay/src/src/main.rs
@@ -0,0 +1,28 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use std::env;
+use tauri::Manager;
+
+fn main() {
+    dotenv::dotenv().ok();
+
+    tauri::Builder::default()
+        .setup(|app| {
+            let main = app.get_window("main").unwrap();
+            let tauri_env = env::var("TAURI_ENV").unwrap_or_else(|_| "production".into());
+
+            // TAURI_ENV gets set during dev but not prod
+            if tauri_env != "development" {
+                let _ = main.eval("window.location.replace('https://dev.artillery-calculator.com')");
+            }
+
+            // use overlay view
+            let _ = main.eval("window.localStorage.setItem('overlay-client', 'true')");
+
+            Ok(())
+        })
+        .invoke_handler(tauri::generate_handler![])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
