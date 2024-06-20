8eddcffa39aa1218f0b99ff548ea8c3678735b9b
Astrid <git@astrid.email>
2024-06-20 09:57:05 +0200

    rename git vars

--- src/hooks/useIsMobile.ts
+++ src/hooks/useIsMobile.ts
@@ -2,15 +2,15 @@ import React from 'react';
 import { UAParser } from 'ua-parser-js';
 
 export default function useIsMobile(): boolean {
-  const [mobile, setMobile] = React.useState<boolean>(false);
+  const [isMobile, setIsMobile] = React.useState<boolean>(false);
 
   React.useEffect(() => {
     const { userAgent } = window.navigator;
 
     const device = new UAParser(userAgent || '').getDevice();
 
-    setMobile(device.type === 'mobile');
-  }, [setMobile]);
+    setIsMobile(device.type === 'mobile');
+  }, [setIsMobile]);
 
-  return mobile;
+  return isMobile;
 }
