e1786a75d89186a6b6bdb1a3a55fd80552883d40
Astrid <git@astrid.email>
2024-06-21 15:52:31 +0200

    raise version check interval to 2 minutes

--- src/components/organisms/VersionAlert.tsx
+++ src/components/organisms/VersionAlert.tsx
@@ -22,12 +22,15 @@ export default function VersionAlert({
     // with that in mind, we fetch the /api/version endpoint which always returns
     // the latest version production is on
 
-    const interval = setInterval(async () => {
-      const remoteVersion = await getRemoteVersion();
+    const interval = setInterval(
+      async () => {
+        const remoteVersion = await getRemoteVersion();
 
-      if (remoteVersion && remoteVersion !== currentVersion)
-        setUpdateAvailable(true);
-    }, 60 * 1_000);
+        if (remoteVersion && remoteVersion !== currentVersion)
+          setUpdateAvailable(true);
+      },
+      60 * 2 * 1_000,
+    );
 
     return () => clearInterval(interval);
   });
