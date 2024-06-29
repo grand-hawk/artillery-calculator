c9e7827921a198da9b2ac680cf1ce9e40f16c07b
Astrid <git@astrid.email>
2024-06-29 13:22:12 +0200

    convert to normal function

--- src/utils/onRenderCallback.ts
+++ src/utils/onRenderCallback.ts
@@ -2,14 +2,15 @@ import type React from 'react';
 
 type onRender = React.ProfilerProps['onRender'];
 
-const onRenderCallback: (
+export default function onRenderCallback(
   ...args: Parameters<onRender>
-) => ReturnType<onRender> = (id, phase, actualDuration) =>
+): ReturnType<onRender> {
+  const [id, phase, actualDuration] = args;
+
   console.log(
     '[Profiler]',
     `${id} took ${actualDuration}ms`,
     `(phase: ${phase})`,
     `(now: ${performance.now()})`,
   );
-
-export default onRenderCallback;
+}
