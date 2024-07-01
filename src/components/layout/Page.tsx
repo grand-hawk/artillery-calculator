0edf1e820fd9252dbf309b4ce779f4ece54d235c
Astrid <git@astrid.email>
2024-07-01 02:26:21 +0200

    remove preview warning

--- src/components/layout/Page.tsx
+++ src/components/layout/Page.tsx
@@ -2,7 +2,6 @@ import Box from '@mui/joy/Box';
 import React from 'react';
 import { useIsClient } from 'usehooks-ts';
 
-import PreviewWarning from '@/components/organisms/PreviewWarning';
 import Theme from '@/components/utils/Theme';
 import useIsOverlay from '@/hooks/useIsOverlay';
 import OverlayView from '@/views/Overlay';
@@ -12,8 +11,6 @@ import type { PropsWithChildren } from 'react';
 export default function Page({ children }: PropsWithChildren) {
   const isClient = useIsClient();
   const isOverlay = useIsOverlay();
-  const isPreview = process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview';
-
   return (
     <Theme>
       {/* only render page if client, site breaks otherwise */}
@@ -26,24 +23,13 @@ export default function Page({ children }: PropsWithChildren) {
               minHeight: '100svh',
               maxWidth: '100svw',
 
+              padding: 4,
+
+              // setting to grid somehow makes the child 100% height and height: 100% doesnt lol
               display: 'grid',
-              gridTemplateRows: 'min-content 1fr',
             }}
           >
-            <Box aria-roledescription="preview warning container">
-              {isPreview && <PreviewWarning />}
-            </Box>
-
-            <Box
-              sx={{
-                padding: 4,
-
-                // setting to grid somehow makes the child 100% height and height: 100% doesnt lol
-                display: 'grid',
-              }}
-            >
-              {children}
-            </Box>
+            {children}
           </Box>
         ))}
     </Theme>
