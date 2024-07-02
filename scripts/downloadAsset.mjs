a87aef016d8b1fc563243cf865e5f10d3801c214
astrid <git@astrid.email>
2024-07-02 17:32:04 +0200

    download asset

---
+++ scripts/downloadAsset.mjs
@@ -0,0 +1,17 @@
+import fs from 'node:fs';
+import path from 'node:path';
+
+const tmp = path.resolve('.tmp');
+if (!fs.existsSync(tmp)) fs.mkdirSync(tmp, { recursive: true });
+
+const id = process.argv[2];
+if (!id) throw new Error('no id supplied');
+
+const response = await fetch(
+  `https://assetdelivery.roblox.com/v1/asset/?id=${id}`,
+);
+if (!response.ok) throw new Error(response.statusText);
+
+const arrayBuffer = await response.arrayBuffer();
+
+fs.writeFileSync(path.join(tmp, `${Date.now()}`), Buffer.from(arrayBuffer));
