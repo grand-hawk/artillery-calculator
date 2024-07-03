5481b0fb7be0c4f8b47af72109b2fdbb866509d9
astrid <git@astrid.email>
2024-07-04 00:52:29 +0200

    comments

--- src/hooks/data/useProjectile.ts
+++ src/hooks/data/useProjectile.ts
@@ -8,9 +8,11 @@ export default function useProjectile(): Projectile {
   const setProjectile = useDataStore((s) => s.setProjectile);
 
   const gun = guns[projectileData.gunKey];
+  // gun doesn't exist anymore, reset to default
   if (!gun) setProjectile(Object.keys(guns)[0], 0);
 
   const projectile = gun.projectiles[projectileData.index];
+  // projectile doesn't exist anymore, reset to the first
   if (!projectile) setProjectile(projectileData.gunKey, 0);
 
   return projectile;
