b6a917d8ca920c21fe8120fe388c811a47306f99
astrid <git@astrid.email>
2024-07-04 00:55:41 +0200

    refresh page when bug fix applied

--- src/hooks/data/useProjectile.ts
+++ src/hooks/data/useProjectile.ts
@@ -1,19 +1,29 @@
+import { useRouter } from 'next/router';
+
 import { guns } from '@/config/projectiles';
 import { useDataStore } from '@/stores/data';
 
 import type { Projectile } from '@/config/projectiles';
 
 export default function useProjectile(): Projectile {
+  const router = useRouter();
+
   const projectileData = useDataStore((s) => s.projectile);
   const setProjectile = useDataStore((s) => s.setProjectile);
 
   const gun = guns[projectileData.gunKey];
   // gun doesn't exist anymore, reset to default
-  if (!gun) setProjectile(Object.keys(guns)[0], 0);
+  if (!gun) {
+    setProjectile(Object.keys(guns)[0], 0);
+    router.reload();
+  }
 
   const projectile = gun.projectiles[projectileData.index];
   // projectile doesn't exist anymore, reset to the first
-  if (!projectile) setProjectile(projectileData.gunKey, 0);
+  if (!projectile) {
+    setProjectile(projectileData.gunKey, 0);
+    router.reload();
+  }
 
   return projectile;
 }
