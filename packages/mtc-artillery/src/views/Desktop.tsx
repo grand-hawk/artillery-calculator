4bde58bcfe9a294c427095ccccb70dd41428d79e
astrid <git@astrid.email>
2024-07-19 16:32:06 +0200

    remove konata

--- packages/mtc-artillery/src/views/Desktop.tsx
+++ packages/mtc-artillery/src/views/Desktop.tsx
@@ -1,12 +1,10 @@
 import Box from '@mui/joy/Box';
 import Typography from '@mui/joy/Typography';
-import { useRouter } from 'next/router';
 import { useTranslations } from 'next-intl';
 import React from 'react';
 
 import ColumnContainer from '@/components/atoms/ColumnContainer';
 import ConfigurationGroup from '@/components/atoms/ConfigurationGroup';
-import Konata from '@/components/atoms/Konata';
 import AzimuthValue from '@/components/organisms/configuration/Azimuth';
 import DistanceValue from '@/components/organisms/configuration/Distance';
 import ElevationValue from '@/components/organisms/configuration/Elevation';
@@ -22,72 +20,66 @@ import { usePropStore } from '@/stores/props';
 export default function DesktopView() {
   const t = useTranslations();
 
-  const { locale } = useRouter();
-
   const version = usePropStore((s) => s.version);
   const motd = usePropStore((s) => s.motd);
 
   return (
-    <>
+    <Box
+      className="desktop"
+      sx={{
+        display: 'grid',
+        gridTemplateRows: '1fr min-content',
+        rowGap: 4,
+      }}
+      zIndex={1}
+    >
       <Box
-        className="desktop"
         sx={{
-          display: 'grid',
-          gridTemplateRows: '1fr min-content',
-          rowGap: 4,
+          display: 'flex',
+          justifyContent: 'center',
         }}
-        zIndex={1}
       >
-        <Box
-          sx={{
-            display: 'flex',
-            justifyContent: 'center',
-          }}
-        >
-          <Box>
-            <Box
-              sx={{
-                display: 'grid',
-                gridTemplateColumns: '1fr 1fr',
+        <Box>
+          <Box
+            sx={{
+              display: 'grid',
+              gridTemplateColumns: '1fr 1fr',
 
-                maxWidth: {
-                  sm: '90vw',
-                  md: '80vw',
-                  lg: '75vw',
-                },
+              maxWidth: {
+                sm: '90vw',
+                md: '80vw',
+                lg: '75vw',
+              },
 
-                padding: 4,
-                gap: 4,
-              }}
-            >
-              <Canvas />
+              padding: 4,
+              gap: 4,
+            }}
+          >
+            <Canvas />
 
-              <ColumnContainer>
-                <Motd message={motd || undefined} />
+            <ColumnContainer>
+              <Motd message={motd || undefined} />
 
-                <ConfigurationGroup>
-                  <ElevationValue />
-                  <AzimuthValue />
-                  <DistanceValue />
-                  <TimeOfFlightValue />
-                  <ProjectileSelection />
-                  <MapSelection />
-                </ConfigurationGroup>
+              <ConfigurationGroup>
+                <ElevationValue />
+                <AzimuthValue />
+                <DistanceValue />
+                <TimeOfFlightValue />
+                <ProjectileSelection />
+                <MapSelection />
+              </ConfigurationGroup>
 
-                <Typography sx={{ marginTop: 'auto' }}>
-                  {t('typography.instructions')}
-                </Typography>
+              <Typography sx={{ marginTop: 'auto' }}>
+                {t('typography.instructions')}
+              </Typography>
 
-                <OverlayCard />
-              </ColumnContainer>
-            </Box>
+              <OverlayCard />
+            </ColumnContainer>
           </Box>
         </Box>
-
-        <Footer version={version!} />
       </Box>
 
-      {locale === 'lolcat-US' && <Konata />}
-    </>
+      <Footer version={version!} />
+    </Box>
   );
 }
