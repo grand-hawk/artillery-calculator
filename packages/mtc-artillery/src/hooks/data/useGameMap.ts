7371262f6a5fe37d88baadf738aefabd6d1031aa
astrid <git@astrid.email>
2025-01-01 21:28:56 +0100

    reset map id if not found

--- packages/mtc-artillery/src/hooks/data/useGameMap.ts
+++ packages/mtc-artillery/src/hooks/data/useGameMap.ts
@@ -1,10 +1,14 @@
-import { gameMaps } from '@/config/maps';
+import { defaultMapId, gameMaps } from '@/config/maps';
 import { useDataStore } from '@/stores/data';
 
 import type { GameMap } from '@/config/maps';
 
 export default function useGameMap(): GameMap {
   const mapId = useDataStore((s) => s.mapId);
+  const setMapId = useDataStore((s) => s.setMapId);
+
   const gameMap = gameMaps[mapId];
+  if (!gameMap) setMapId(defaultMapId);
+
   return gameMap;
 }
