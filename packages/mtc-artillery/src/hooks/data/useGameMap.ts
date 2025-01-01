425280ddaa5ce909acebc01b290ffe39088a95fe
astrid <git@astrid.email>
2025-01-01 21:33:36 +0100

    +reload router

--- packages/mtc-artillery/src/hooks/data/useGameMap.ts
+++ packages/mtc-artillery/src/hooks/data/useGameMap.ts
@@ -1,14 +1,21 @@
+import { useRouter } from 'next/router';
+
 import { defaultMapId, gameMaps } from '@/config/maps';
 import { useDataStore } from '@/stores/data';
 
 import type { GameMap } from '@/config/maps';
 
 export default function useGameMap(): GameMap {
+  const router = useRouter();
+
   const mapId = useDataStore((s) => s.mapId);
   const setMapId = useDataStore((s) => s.setMapId);
 
   const gameMap = gameMaps[mapId];
-  if (!gameMap) setMapId(defaultMapId);
+  if (!gameMap) {
+    setMapId(defaultMapId);
+    router.reload();
+  }
 
   return gameMap;
 }
